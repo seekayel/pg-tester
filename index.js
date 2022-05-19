cd5a88af998f86a37570514fb923179f2a46d3da
Kam Lasater <ckl@seekayel.com>
2022-05-18 21:37:05 -0400

    Actually working

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-
+const {Pool, Client} = require('pg')
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+const connString = process.env.DATABASE_URL
+
+// let pool = null
+
+app.all('*', async (req, res) => {
+  console.log(JSON.stringify(process.env.NODE_EXTRA_CA_CERTS,null,2))
+  console.log(`Request on: ${req.path}`)
+  // if(!pool){
+  //   console.log(`Trying: ${connString}`)
+  //   pool = await new Pool({connString});
+  //   console.log(JSON.stringify(pool,null,2))
+  // }
+
+  // console.log(`Trying: query`)
+  // let now = await pool.query("SELECT NOW()");
+
+  const client = new Client({connString});
+  await client.connect();
+
+  console.log('client demo: query')
+  const now = await client.query("SELECT NOW()");
+  const data = JSON.stringify(now.rows[0].now,null,2)
+
+  console.log(`Just queried ${data}`)
+  res.send(`The time is now: ${data}`)
 })
 app.listen(process.env.PORT || 3000)
