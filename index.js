e9aed557b2717a220672544627d5f15962801112
Kam Lasater <ckl@seekayel.com>
2022-05-20 12:41:34 -0400

    Added more info, better routes and file paths

--- index.js
+++ index.js
@@ -5,45 +5,73 @@ const express = require('express')
 const { resolve } = require('path')
 const app = express()
 
-const connString = "" //process.env.DATABASE_URL
 
-// let pool = null
-
-app.get('/ca-cert', async (req,res) => {
+app.get('/file/ca-cert', async (req,res) => {
+  console.log(`got ${req.path}`)
   res.sendFile(path.resolve(process.env.NODE_EXTRA_CA_CERTS))
 })
-
+app.get('/file/global-bundle', async (req,res) => {
+  console.log(`got ${req.path}`)
+  res.sendFile(path.resolve('./_cyclic/global-bundle.pem'))
+})
+app.get('/file/postgres-root', async (req,res) => {
+  console.log(`got ${req.path}`)
+  res.sendFile(path.resolve('~/.postgresql/root.crt'))
+})
 
 async function query(config) {
   const client = new Client(config);
   await client.connect();
 
   console.log(`Client with config: ${JSON.stringify(config,null,2)}`)
+  const isSSL = await client.query("show ssl");
+
   const now = await client.query("SELECT NOW()");
   // console.log(`Queried with: ${JSON.stringify(client,null,2)}`)
   console.log(`TLS Options: ${(client.connection?.stream?._tlsOptions)? 'Configured':'Missing'}`)
-  return JSON.stringify(now.rows[0].now,null,2)
+  return JSON.stringify({...now.rows[0],...isSSL.rows[0]},null,2)
 }
 
 
-app.get('/with-ssl', async (req,res) => {
-  console.log('got /with-ssl')
 
-  const config = {
-    ssl: {
-      rejectUnauthorized: true,
-      ca: fs.readFileSync(path.resolve(process.env.PGSSLROOTCERT)).toString(),
-    },
-  }
+app.get('/env/insecure', async (req,res) => {
+  console.log(`got ${req.path}`)
 
-  const data = await query(config)
+  process.env.PGHOST        = process.env.secure_PGHOST
+  process.env.PGPORT        = process.env.secure_PGPORT
+  process.env.PGDATABASE    = process.env.secure_PGDATABASE
+  process.env.PGUSER        = process.env.secure_PGUSER
+  process.env.PGPASSWORD    = process.env.secure_PGPASSWORD
+  process.env.PGSSLMODE     = 'disable'
+  process.env.PGSSLROOTCERT = ''
+
+
+  const data = await query({})
 
   console.log(`Just queried ${data}`)
   res.send(`The time is now: ${data}`)
 })
 
-app.get('/with-ssl-string', async (req,res) => {
-  console.log('got /with-ssl')
+app.get('/env/ssl', async (req,res) => {
+  console.log(`got ${req.path}`)
+
+  process.env.PGHOST        = process.env.secure_PGHOST
+  process.env.PGPORT        = process.env.secure_PGPORT
+  process.env.PGDATABASE    = process.env.secure_PGDATABASE
+  process.env.PGUSER        = process.env.secure_PGUSER
+  process.env.PGPASSWORD    = process.env.secure_PGPASSWORD
+  process.env.PGSSLMODE     = process.env.secure_PGSSLMODE
+  process.env.PGSSLROOTCERT = process.env.secure_PGSSLROOTCERT
+
+
+  const data = await query({})
+
+  console.log(`Just queried ${data}`)
+  res.send(`The time is now: ${data}`)
+})
+
+app.get('/string/ssl', async (req,res) => {
+  console.log(`got ${req.path}`)
 
   const config = {
     connectionString: process.env.DATABASE_URL_SECURE,
@@ -59,8 +87,8 @@ app.get('/with-ssl-string', async (req,res) => {
   res.send(`The time is now: ${data}`)
 })
 
-app.get('/with-string', async (req,res) => {
-  console.log('got /with-string')
+app.get('/string/insecure', async (req,res) => {
+  console.log(`got ${req.path}`)
 
   const config = {
     connectionString: process.env.DATABASE_URL_INSECURE,
@@ -73,24 +101,17 @@ app.get('/with-string', async (req,res) => {
 })
 
 app.get('/info', (req,res) => {
-  res.json({env: process.env, resolve: path.resolve('.')})
+  console.log(`got ${req.path}`)
+  res.json({
+    // env: process.env,
+    resolve: path.resolve('.'),
+    pgcrt: path.resolve('~/.postgresql/root.crt'),
+  })
 })
 
 app.all('*', async (req, res) => {
-  // console.log(JSON.stringify(process.env,null,2))
-  console.log(`Request on: ${req.path}`)
-  // if(!pool){
-  //   console.log(`Trying: ${connString}`)
-  //   pool = await new Pool({connString});
-  //   console.log(JSON.stringify(pool,null,2))
-  // }
-
-  // console.log(`Trying: query`)
-  // let now = await pool.query("SELECT NOW()");
-
-  let data = await query({})
-  console.log(`Just queried ${data}`)
-  res.send(`The time is now: ${data}`)
-
+  console.log(`got ${req.path}`)
+  res.send('mic check, good.')
 })
+
 app.listen(process.env.PORT || 3000)
